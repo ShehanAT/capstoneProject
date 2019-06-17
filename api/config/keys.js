6d961df09a5b3c5ad38ce7dfc7a8f92e6f32f19d
Shehan Atuk <shehanatukorala@gmail.com>
2019-06-17 13:28:33 -0400

    Update keys.js

--- api/config/keys.js
+++ api/config/keys.js
@@ -1,4 +1,4 @@
-const flickrApiKey = "06e92a1bf665b9d7d6dfb4883c33a4c2";
-const flickrApiSecret = "3b9ccd35e889501e";
+const flickrApiKey = "<InsertFlickrAPIKeyHere>";
+const flickrApiSecret = "<InsertFlickrAPISecretHere>";
 module.exports.flickrApiKey = flickrApiKey;
 module.exports.flickrApiSecret = flickrApiSecret;
