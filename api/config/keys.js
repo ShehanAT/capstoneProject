9acfb2373d082a6ac1582fb4120e7894a9361929
Shehan Atuk <shehanatukorala@gmail.com>
2019-06-17 13:34:06 -0400

    Update keys.js

--- api/config/keys.js
+++ api/config/keys.js
@@ -1,4 +1,6 @@
 const flickrApiKey = "<InsertFlickrAPIKeyHere>";
 const flickrApiSecret = "<InsertFlickrAPISecretHere>";
+const AZURECOMPUTERVISIONSUBSCRIPTIONKEY = "<InsertMicrosoftAzureCognitiveServicesComputerVisionKeyHere>";
 module.exports.flickrApiKey = flickrApiKey;
 module.exports.flickrApiSecret = flickrApiSecret;
+module.exports.AZURECOMPUTERVISIONSUBSCRIPTIONKEY = AZURECOMPUTERVISIONSUBSCRIPTIONKEY;
